8f86ada9277c761f63b22a6a40016224a4eb5bee
gauravm-bit <gauravwrk01@gmail.com>
2020-04-23 02:24:10 +0530

    ADDED:testcase for adding 1 litre water and 1000 ml

--- app/Volume.js
+++ app/Volume.js
@@ -1,7 +1,13 @@
 class VolumeFactory
 {
     gallonToLitreConverter = (volume) => (volume * 3.78);
+
+    litreToMilliLitreConverter = (volume) => (volume * 1000);
+
+    millilitretoLitreConverter = (volume) => (volume / 1000);
     
     returnSame = (volume) => volume;
+
+    additionVolume = (volume1, volume2) => volume1 + volume2;
 }
 module.exports = new VolumeFactory;
